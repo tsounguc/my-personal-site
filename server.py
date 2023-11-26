bc4cfd814257874201a6058bdab425aa2d38d4f9
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-26 14:40:46 -0500

    angela's cv website

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
+@app.route("/")
+def home(name=None):
+    return render_template('angela.html', name=name)
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
